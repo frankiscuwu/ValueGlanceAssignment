b5ca4423276f894b91a0ddef4306a67e2702f81e
Frank Yang <frankyang009@gmail.com>
2025-01-07 21:52:06 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import AAPL from './AAPL';
+
+const App = () => (
+  <div className="min-h-screen flex flex-col items-center justify-start p-6 w-screen">
+    <h1 className="text-2xl font-bold mb-4 text-center">AAPL Financial Data Filtering</h1>
+    <AAPL />
+  </div>
+);
+
+export default App;
